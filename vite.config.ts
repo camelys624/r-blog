8b229799287ac54aaa9937dfd4b58ec12e494234
camel_y <camel_yangz@163.com>
2026-01-26 22:22:40 +0800

    build: 添加nitro包修复vercel报404错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -11,8 +11,10 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
-    react(),
     tailwindcss(),
+    nitro({
+      preset: 'vercel'
+    })
   ],
   ssr: {
     external: ['@prisma/client', '@prisma/adapter-pg', 'pg'],
